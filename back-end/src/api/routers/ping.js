bff614b6d93778095a08d2e2f46a88c379cb55c9
gabrieldeori <deori.pessoal@gmail.com>
2022-01-25 17:12:02 -0300

    fix: add module.exports = router; faltando

--- back-end/src/api/routers/ping.js
+++ back-end/src/api/routers/ping.js
@@ -1,7 +1,12 @@
 const { Router } = require('express');
 
+const controllers = require('../../database/controllers');
+
 const router = Router();
 
 router.get(
   '/',
+  controllers.ping,
   );
+
+module.exports = router;

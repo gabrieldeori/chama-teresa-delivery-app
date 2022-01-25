cf0e64fd1d69b278f87191bce466fba5bc0a1ef0
gabrieldeori <deori.pessoal@gmail.com>
2022-01-25 19:10:14 -0300

    feat: melhorando estrutura padrao controllers, trycatch e middleware de erros

--- back-end/src/database/controllers/ping.js
+++ back-end/src/database/controllers/ping.js
@@ -1,5 +1,15 @@
-function ping(_req, res) {
-  return res.status(200).json({ message: 'Pong'});
+const services = require('../services');
+const { status } = require('../utils');
+
+async function ping(_req, res, nex) { // Async Await caso seja necessário esperar resposta. :)
+  try {
+    const response = await services.ping();
+    if (response.error) return nex(response);
+    return res.status(status.OK).json({ message: response.message });
+  } catch (e) {
+    console.log(e);
+    return nex({ error: e, status: status.INTERNAL_SERVER_ERROR });
+  }
 }
 
 module.exports = ping;

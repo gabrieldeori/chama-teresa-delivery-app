d945767904c4775c2539b6544de25b1a2cc1ea64
gabrieldeori <deori.pessoal@gmail.com>
2022-01-26 12:03:04 -0300

    feat: add serviço para resgatar todos produtos

---
+++ back-end/src/database/services/products.js
@@ -0,0 +1,13 @@
+const models = require('../models');
+const { errors, status } = require('../utils');
+
+async function getAll() {
+  const products = await models.Product.findAll();
+  if (!products || products.length <= 0) return errors.noProducts;
+  const sendToFrontEnd = { products };
+  return { sendToFrontEnd, statusCode: status.OK };
+}
+
+module.exports = {
+  getAll,
+};

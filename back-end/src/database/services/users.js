2cb46e95be18ddbbc979c8eb7f30b3088841d655
gabrieldeori <deori.pessoal@gmail.com>
2022-01-26 11:03:53 -0300

    feat: add service de create para user

--- back-end/src/database/services/users.js
+++ back-end/src/database/services/users.js
@@ -1,13 +1,21 @@
 const models = require('../models');
-const { authorization, errors, status } = require('../utils');
+const { authorization, errors, roles, status } = require('../utils');
 
-async function findEmail(email) {
-  const response = await models.User.findOne({ where: { email } });
-  return response;
+async function create({ name, email, password }) {
+  const userExists = await models.User.findOne({ where: { email } });
+  if (userExists) return errors.userExists;
+  const role = roles.customer;
+  const newUser = { name, email, password, role };
+  const { id } = await models.User.create(newUser);
+  if (!id) return errors.dbError;
+  const dataToToken = { email, name, role };
+  const token = authorization.create(dataToToken);
+  const sendToFrontEnd = { id, token, ...dataToToken };
+  return { sendToFrontEnd, statusCode: status.CREATED };
 }
 
-async function login(email, password) {
-  const userExists = await findEmail(email);
+async function login({ email, password }) {
+  const userExists = await models.User.findOne({ where: { email } });
   if (!userExists) return errors.userNonexistent;
   if (password !== userExists.password) return errors.invalidPassword;
   const { name, role, dataValues: { id } } = userExists;
@@ -18,5 +26,6 @@ async function login(email, password) {
 }
 
 module.exports = {
+  create,
   login,
 };

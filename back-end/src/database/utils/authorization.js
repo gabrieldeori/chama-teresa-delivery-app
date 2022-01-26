a170a2cb8c4cfce7f126b3128a26243045499484
gabrieldeori <deori.pessoal@gmail.com>
2022-01-26 13:38:44 -0300

    feat: add funcao authenticate em authorization utils

--- back-end/src/database/utils/authorization.js
+++ back-end/src/database/utils/authorization.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const jwt = require('jsonwebtoken');
 
+const errors = require('./errors');
+
 const pathToKey = './jwt.evaluation.key';
 const secret = fs.readFileSync(pathToKey, 'utf-8');
 
@@ -14,6 +16,17 @@ function create(data) {
   return token;
 }
 
+function verify(token) {
+  if (!token) return errors.tokenNotFound;
+  try {
+    jwt.verify(token, secret);
+    return { ok: true };
+  } catch (_e) {
+    return errors.tokenInvalidOrExpired;
+  }
+}
+
 module.exports = {
   create,
+  verify,
 };

16000f85813589288ea00e8e295b6b7f50266f19
gabrieldeori <deori.pessoal@gmail.com>
2022-01-26 12:03:33 -0300

    feat: add erro de noProducts em errors

--- back-end/src/database/utils/errors.js
+++ back-end/src/database/utils/errors.js
@@ -1,10 +1,11 @@
 const status = require('./status');
 
 const errors = {
+  dbError: { error: 'Erro no banco de dados', status: status.INTERNAL_SERVER_ERROR },
+  invalidPassword: { error: 'Senha incorreta', statusCode: status.UNAUTHORIZED },
+  noProducts: { error: 'Nenhum produto cadastrado', statusCode: status.NOT_FOUND },
   userNonexistent: { error: 'Usuário não existe', statusCode: status.NOT_FOUND },
   userExists: { error: 'Usuário já existe', statusCode: status.CONFLICT },
-  invalidPassword: { error: 'Senha incorreta', statusCode: status.UNAUTHORIZED },
-  dbError: { error: 'Erro no banco de dados', status: status.INTERNAL_SERVER_ERROR },
 };
 
 module.exports = errors;

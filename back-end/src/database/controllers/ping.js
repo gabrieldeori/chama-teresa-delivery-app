896f1f6f65d25cb06cf2a74e2099d2c6c534f4ec
gabrieldeori <deori.pessoal@gmail.com>
2022-01-27 16:21:47 -0300

    fix: Teste mostrou que try catch era desnecessário em ping.js

--- back-end/src/database/controllers/ping.js
+++ back-end/src/database/controllers/ping.js
@@ -2,17 +2,12 @@ const services = require('../services');
 const utils = require('../utils');
 
 async function ping(_req, res, nex) { // Async Await caso seja necessário esperar resposta. :)
-  try {
-    const response = await services.ping();
+  const response = await services.ping();
 
-    if (response.error) return nex(response);
-    return res
-      .status(response.statusCode)
-      .json(response.sendToFrontEnd);
-  } catch (e) {
-    console.log(e);
-  }
-  return nex(utils.errors.internalServerError);
+  if (response.error) return nex(response);
+  return res
+    .status(response.statusCode)
+    .json(response.sendToFrontEnd);
 }
 
 module.exports = ping;
